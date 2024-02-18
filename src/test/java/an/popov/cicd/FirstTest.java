a0a474a20987acebedf39c12fdb2b75ec1982ab6
Андрей Попов <123795701+garri1120@users.noreply.github.com>
2024-02-18 18:31:11 +0300

    add test

---
+++ src/test/java/an/popov/cicd/FirstTest.java
@@ -0,0 +1,19 @@
+package an.popov.cicd;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class FirstTest {
+  @Test
+  void test1(){
+    String string = "Hello";
+    Assertions.assertEquals("Hello", string);
+  }
+
+  @Test
+  void test2(){
+    String string = "Hello";
+    Assertions.assertEquals("Hello world", string);
+  }
+}
